56ee4243adce241dfd9130406e21326330a8e7e8
Roonie <roonierue@gmail.com>
2021-09-20 20:57:18 +0200

    adding categories

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 
 const mealResult = document.getElementById('display-meals')
-    randomMeal = document.getElementById('random-meal')
-    moreMeals = document.getElementById('try-again')
+      randomMeal = document.getElementById('single-meal')
+      foodCategory = document.getElementById('food-category')
 
 randomMeal.addEventListener('click', getMeal);
-moreMeals.addEventListener('click', );
+category.addEventListener('click', categoryMeal);
 
 
 function getMeal(){
@@ -16,14 +16,40 @@ function getMeal(){
         mealResult.innerHTML = data.meals.map(meal =>
             `<h2 class="mt-5 text-center">Get inspired by : ${meal.strMeal} </h2>
             <div>
-                <img src="${meal.strMealThumb}" class="rounded mx-auto d-block"/>
+                <img src="${meal.strMealThumb}" class="rounded d-block mx-auto"/>
             </div>
         
-            `)
+            `)       
     })
     .catch(function(err) {
         console.log(err);
+    });     
+}
+
+function categoryMeal(){
+
+    fetch('meals.json')
+    .then(res => res.json())
+    .then(data =>{
+        console.log(data);
+
+        let output = '';
+
+        data.forEach(function(categories){
+                    output += `
+            <h3 class="text-center">${categories.strCategory}</h3>
+            <div>
+                <img src="${categories.strCategoryThumb}" class="img-fluid"/>
+            </div>
+            `}); 
+
+        foodCategory.innerHTML = output;
+        })
+    
+    .catch(function(err) {
+        console.log(err);
     });
+
     
 }
 
