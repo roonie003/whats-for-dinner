e0ef0d43bbad985f4d152b1df03d23e73aeeb3b5
Roonie <roonierue@gmail.com>
2021-09-22 20:16:20 +0200

    added css page for the categories

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 
-const mealResult = document.getElementById('display-meals')
-      randomMeal = document.getElementById('single-meal')
-      foodCategory = document.getElementById('food-category')
-
-randomMeal.addEventListener('click', getMeal);
-category.addEventListener('click', categoryMeal);
-
+const mealResult = document.getElementById('display-meal'),
+      mealResults = document.getElementById('display-meals'),
+      randomMeal = document.getElementById('random-meal'),
+      foodCategory = document.getElementById('category');
 
 function getMeal(){
 
@@ -14,17 +11,30 @@ function getMeal(){
     .then(data =>{
         console.log(data);
         mealResult.innerHTML = data.meals.map(meal =>
-            `<h2 class="mt-5 text-center">Get inspired by : ${meal.strMeal} </h2>
-            <div>
-                <img src="${meal.strMealThumb}" class="rounded d-block mx-auto"/>
+            `
+            <div class="card text-center card-body mt-3 mx-auto" style="width:40rem">
+                <div class="row">
+                    <div class="col-md">
+                        <h2 class="mt-3">Get inspired by : ${meal.strMeal} </h2>
+                    <div>
+                        <img src="${meal.strMealThumb}" alt="${meal.strMeal}" class="img-fluid mb-5 rounded"/>
+                    </div>
+                </div>
             </div>
-        
-            `)       
+
+            
+            `
+            )       
     })
     .catch(function(err) {
         console.log(err);
-    });     
+    });    
+
+
 }
+
+
+
 
 function categoryMeal(){
 
@@ -37,19 +47,35 @@ function categoryMeal(){
 
         data.forEach(function(categories){
                     output += `
-            <h3 class="text-center">${categories.strCategory}</h3>
-            <div>
-                <img src="${categories.strCategoryThumb}" class="img-fluid"/>
-            </div>
+
+                <div class="card-group style="width:40rem">
+                    <div class="card">
+                      <img class="card-img-top" src="${categories.strCategoryThumb}" alt="Card image cap"/>
+                      <div class="card-body">
+                        <h4 class="card-title">${categories.strCategory}</h4>
+                      </div>
+                    </div>
+                </div>
             `}); 
 
-        foodCategory.innerHTML = output;
+        mealResults.innerHTML = output;
         })
     
     .catch(function(err) {
         console.log(err);
     });
-
-    
 }
+randomMeal.addEventListener('click', getMeal);
+foodCategory.addEventListener('click', categoryMeal);
+
+
+
+
+
+
+
+
+
+
+
 
